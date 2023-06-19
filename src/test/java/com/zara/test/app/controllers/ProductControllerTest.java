d8795e9de3cfc3653e0bbb4ff84191c11b1ece24
José Pascual Gimeno Marí <tirantvcia@gmail.com>
2023-06-19 19:29:06 +0200

    refactor, unused imports

--- src/test/java/com/zara/test/app/controllers/ProductControllerTest.java
+++ src/test/java/com/zara/test/app/controllers/ProductControllerTest.java
@@ -1,9 +1,9 @@
 package com.zara.test.app.controllers;
 
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +11,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
 
 import com.zara.test.app.dto.TestData;
 import com.zara.test.app.services.ProductService;
