ecee22a17468245bca5628a59ad236f4ef4ed291
José Pascual Gimeno Marí <tirantvcia@gmail.com>
2023-06-21 00:08:08 +0200

    circuitbreaker implementation

--- src/main/java/com/zara/test/app/controllers/ProductController.java
+++ src/main/java/com/zara/test/app/controllers/ProductController.java
@@ -1,9 +1,12 @@
 package com.zara.test.app.controllers;
 
+import java.net.ConnectException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.zara.test.app.dto.Product;
 import com.zara.test.app.services.ProductService;
+
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
 @RequestMapping("/product/")
@@ -23,6 +29,7 @@ public class ProductController implements SwaggerProductController {
 	@Override
 	@GetMapping("/{productId}")
 	@ResponseStatus(HttpStatus.OK)
+	@CircuitBreaker(name = "mockproduct")
 	public Product getDetails(@PathVariable(name="productId") Long id) {
 		return service.getProductDetails(id);
 	}
@@ -30,7 +37,9 @@ public class ProductController implements SwaggerProductController {
 	@Override
 	@GetMapping("/{productId}/similar")
 	@ResponseStatus(HttpStatus.OK)
+	@CircuitBreaker(name = "mockproduct")
 	public List<Long> getProductSimilarIds(@PathVariable(name="productId") Long id) {
 		return service.getProductSimilarIds(id);
 	}
+	
 }
