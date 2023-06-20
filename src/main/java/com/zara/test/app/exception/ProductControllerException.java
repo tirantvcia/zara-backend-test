fd70797afccf6d625d2190f756c38b7b652a35a1
José Pascual Gimeno Marí <tirantvcia@gmail.com>
2023-06-21 00:08:33 +0200

    ExceptionHandling implementation

---
+++ src/main/java/com/zara/test/app/exception/ProductControllerException.java
@@ -0,0 +1,27 @@
+package com.zara.test.app.exception;
+
+import java.net.ConnectException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+
+@ControllerAdvice
+public class ProductControllerException {
+	@ExceptionHandler(CallNotPermittedException.class)
+	@ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
+	public ResponseEntity<String> handleCallNotPermittedException(CallNotPermittedException exception) {
+		return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(exception.getMessage());
+	}
+
+	@ExceptionHandler(ConnectException.class)
+	@ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
+	public ResponseEntity<String> handleConnectException(ConnectException exception) {
+		return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("mockproduct: "+exception.getMessage());
+	}
+
+}
