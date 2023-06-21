07353fe93dc6fe09b1ddc18053c01dae13065c64
tirantvcia <tirantvcia@gmail.com>
2023-06-21 08:24:35 +0200

    refactor unused imports

--- src/main/java/com/zara/test/app/controllers/ProductController.java
+++ src/main/java/com/zara/test/app/controllers/ProductController.java
@@ -1,12 +1,9 @@
 package com.zara.test.app.controllers;
 
-import java.net.ConnectException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.zara.test.app.dto.Product;
 import com.zara.test.app.services.ProductService;
 
-import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
